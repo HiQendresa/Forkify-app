7caf4ba9fade2725f383ae08e402b21c1458ac5a
HiQendresa <qendresa.braha@outlook.com>
2022-05-19 06:58:09 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "95367edb-f342-4b2f-94a9-d9ea69ef9389";
+export const MODAL_CLOSE_SEC = 2.5;
